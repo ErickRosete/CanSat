f94a40deaa38514cb0e0adb39da7d132fb6782ee
Oscar Rosete <oscaralonso11@hotmail.com>
2019-01-25 01:05:45 -0800

    Se deja ejemplo con 2 ino

--- main.js
+++ main.js
@@ -31,10 +31,80 @@ function createWindow() {
 
 app.on('ready', createWindow);
 
+var cmd = require('node-cmd');
 ipcMain.on("code:submit",(event,informacion)=>{
     // app.quit()
     console.log("logging info")
     console.log(informacion);
+    var ubicacion_cli = __dirname+"\\cli\\arduino-cli.exe"
+    var puerto = "COM8"
+    var board = "arduino:avr:uno"
+    var ubicacion_arduino = "D:\\Program Files (x86)\\Arduino\\Arduino_debug.exe"
+
+    if(informacion=="Primero"){
+        var command= '"' + ubicacion_cli + '" --format json board list'
+        cmd.get(
+            command,
+            function (err, data, stderr) {
+                if (err) {
+                    console.log(err)
+                }
+                else {
+                    console.log(command)
+                    console.log('Operacion exitosa :\n\n', data)
+                    // arduino conectadoa
+                        console.log(JSON.parse(data).serialBoards[0])
+                        if(JSON.parse(data).serialBoards[0]!=undefined){
+                            var board=JSON.parse(data).serialBoards[0].port;
+                            console.log(board)
+                            mainWindow.webContents.send(
+                                'code:feedback',board
+                            )
+                        }
+                    
+                    
+                }
+            }
+        );
+    }
+
+    else if(informacion=="Segundo"){
+        var archivoIno=__dirname+"\\Arduino\\Arduino.ino"
+        console.log("segundo")
+        var carga = '"' + ubicacion_arduino + '" --board ' + board + ' --port ' + puerto + ' --upload ' + archivoIno;
+        cmd.get(
+            carga,
+            function (err, data, stderr) {
+                if (err) {
+                    console.log(err)
+                }
+                else {
+                    console.log(command)
+                    console.log('Operacion exitosa :\n\n', data)                
+                }
+            }
+        );
+    }
+
+    else if(informacion=="Tercero"){
+        var archivoIno=__dirname+"\\Arduino2\\Arduino.ino"
+        console.log("tercero")
+        var carga = '"' + ubicacion_arduino + '" --board ' + board + ' --port ' + puerto + ' --upload ' + archivoIno;
+        cmd.get(
+            carga,
+            function (err, data, stderr) {
+                if (err) {
+                    console.log(err)
+                }
+                else {
+                    console.log(command)
+                    console.log('Operacion exitosa :\n\n', data)                
+                }
+            }
+        );
+    }
+    
+    console.log(`ubicacion es: ${ubicacion_cli}`);
 })
 
 app.on('window-all-closed', function () {
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+// botonmenu
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import Paper from '@material-ui/core/Paper';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Popper from '@material-ui/core/Popper';
+import Grow from '@material-ui/core/Grow';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Menu from '@material-ui/core/Menu';
+
+
 // npm install --save is-electron
 // HemalR had the solution  
 // https://github.com/electron/electron/issues/9920
@@ -10,9 +23,27 @@ import Button from '@material-ui/core/Button';
 import isElectron from 'is-electron';
 
 
+  
 
 
 class HomePage extends Component {
+    // menu
+    // state = {
+    //     open: false,
+    // };
+    state = {
+        anchorEl: null,
+        selectedIndex: 1,
+      };
+    thirdOption="COMZ"
+
+    options = [
+        'Seleccionar un puerto',
+        'COMX',
+        'COMY',
+        this.thirdOption,
+    ];
+
 
     handleClick2=(e)=>{
         e.preventDefault();
@@ -20,6 +51,15 @@ class HomePage extends Component {
         if (isElectron()) {
             console.log(window.ipcRenderer);
             window.ipcRenderer.send('code:submit','Segundo')
+        }
+    }
+
+    handleClick3=(e)=>{
+        e.preventDefault();
+        console.log('El segundo boton fue seleccionado.');
+        if (isElectron()) {
+            console.log(window.ipcRenderer);
+            window.ipcRenderer.send('code:submit','Tercero')
         }
     }
 
@@ -33,8 +73,33 @@ class HomePage extends Component {
         }
     }
 
+    handleClickListItem = event => {
+        this.setState({ anchorEl: event.currentTarget });
+      };
+    
+      handleMenuItemClick = (event, index) => {
+        this.setState({ selectedIndex: index, anchorEl: null });
+      };
+    
+      handleClose = () => {
+        this.setState({ anchorEl: null });
+      };
+
+
+
     render() {
         const { classes } = this.props;
+        // const { open } = this.state;
+        const { anchorEl } = this.state;
+
+
+        window.ipcRenderer.on("code:feedback", (event, arg) => {
+            console.log(arg);
+            this.thirdOption=arg;
+            // this.setState({ options: options });
+            console.log(this.options)
+
+        });
 
         function handleClick1(e) {
             e.preventDefault();
@@ -55,11 +120,45 @@ class HomePage extends Component {
         return (
             <div className="HomePage">
                 <Button variant="contained" color="primary" className={classes.button} onClick={handleClick1}>
-                    Primary </Button>
+                    Escaneo arduino </Button>
                 <Button variant="contained" color="secondary" className={classes.button} onClick={this.handleClick2}>
-                    Secondary </Button>
+                    LED Rapido </Button>
+                <Button variant="contained" color="secondary" className={classes.button} onClick={this.handleClick3}>
+                    LED Lento </Button>
                 <Button variant="contained" color="secondary" className={classes.button} onClick={this.closeClick}>
                     Close </Button>
+                <List component="nav">
+                    <ListItem
+                        button
+                        aria-haspopup="true"
+                        aria-controls="lock-menu"
+                        aria-label="When device is locked"
+                        onClick={this.handleClickListItem}
+                    >
+                        <ListItemText
+                        primary="Puerto"
+                        secondary={this.options[this.state.selectedIndex]}
+                        />
+                    </ListItem>
+                    </List>
+                    <Menu
+                    id="lock-menu"
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={this.handleClose}
+                    >
+                    {this.options.map((option, index) => (
+                        <MenuItem
+                        key={option}
+                        disabled={index === 0}
+                        selected={index === this.state.selectedIndex}
+                        onClick={event => this.handleMenuItemClick(event, index)}
+                        >
+                        {option}
+                        </MenuItem>
+                    ))}
+                    </Menu>
+
             </div>
         );
     }
